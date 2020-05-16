ddc1a17eca0be14660d9a65400691735f5935aa7
sayachan <sayachan@localhost>
2020-05-16 15:16:35 +0900

    betを賭けるところまで完了

--- black_jack2/src/black_jack/MainGame.java
+++ black_jack2/src/black_jack/MainGame.java
@@ -1,97 +1,98 @@
 package black_jack;
 
 import java.util.Scanner;
+
 public class MainGame {
-	  public static void main(String[] args) {
-	    System.out.println("ブラックジャックへようこそ！");
+	public static void main(String[] args) {
+		System.out.println("ブラックジャックへようこそ！");
 
-	    Deck deck = new Deck();
-	    //deck.printAll(); //デバック用
+		Deck deck = new Deck();
+		//deck.printAll(); //デバック用
 
-	    Player player = new Player(); //プレイヤーを作成し初期値を設定
-	    Player dealer = new Player(0,0); //ディーラーを作成し初期値を設定
+		Player player = new Player(); //プレイヤーを作成し初期値を設定
+		Player dealer = new Player(0, 0); //ディーラーを作成し初期値を設定
 
+		try (Scanner scan = new Scanner(System.in);) {
+			while (true) {
+				//プレイヤーは賭け金を設定する
+				System.out.println("賭け金を決めてください");
+				//キーボードの入力を受け付けて、変数strに代入する
+				String str = scan.next();
+				int bet = Integer.parseInt(str);
 
-	    try (Scanner scan = new Scanner(System.in);) {
-	    	while (true) {
-	    	    //プレイヤーは賭け金を設定する
-	    	    System.out.println("賭け金を決めてください");
-		        //キーボードの入力を受け付けて、変数strに代入する
-		        String str = scan.next();
-		        int bet = Integer.parseInt(str);
+				if (bet < player.nowCash()) {
+					break;
+				} else if (bet < 1000) {
+					System.out.println("賭け金は1000以上からです。入力し直してください");
+				} else {
+					System.out.println("賭け金は" + bet + "になりました");
+				}
+			}
+		}
 
-		        if(1000 < bet && player.) {
-		        	break;
-		        }else if(bet < 1000) {
-		        	System.out.println("賭け金は1000以上からです。入力し直してください");
-		        }
-	    	}
-	    }
+		//プレイヤー・ディーラーがカードを2枚引く
+		player.add(deck.pop());
+		dealer.add(deck.pop());
+		player.add(deck.pop());
+		dealer.add(deck.pop());
 
+		//プレイヤー・ディーラーの手札を表示
+		System.out.println("貴方の1枚目のカードは" + player.get(0) + "です");
+		System.out.println("ディーラーの1枚目のカードは" + dealer.get(0) + "です");
+		System.out.println("貴方の2枚目のカードは" + player.get(1) + "です");
+		System.out.println("ディーラーの2枚目のカードは秘密です");
 
-	    //プレイヤー・ディーラーがカードを2枚引く
-	    player.add(deck.pop());
-	    dealer.add(deck.pop());
-	    player.add(deck.pop());
-	    dealer.add(deck.pop());
+		System.out.println("あなたの現在のポイントは" + player.sumPoint() + "です");
 
-	    //プレイヤー・ディーラーの手札を表示
-	    System.out.println("貴方の1枚目のカードは" + player.get(0) + "です");
-	    System.out.println("ディーラーの1枚目のカードは" + dealer.get(0) + "です");
-	    System.out.println("貴方の2枚目のカードは" + player.get(1) + "です");
-	    System.out.println("ディーラーの2枚目のカードは秘密です");
+		//プレイヤーがカードを引くフェーズ
+		try (Scanner scan2 = new Scanner(System.in);) {
+			while (true) {
+				System.out.println("カードを引きますか？ Yes:y or No:n");
+				//キーボードの入力を受け付けて、変数strに代入する
+				String str2 = scan2.next();
 
-	    System.out.println("あなたの現在のポイントは" + player.sumPoint() + "です");
+				if ("n".equals(str2)) {
+					break;
+				} else if ("y".equals(str2)) {
+					//手札に山札から1枚加える
+					int index = player.add(deck.pop());
+					System.out.println("あなたの" + (index + 1) + "枚目のカードは" + player.get(index) + "です");
+					System.out.println("現在の合計は" + player.sumPoint() + "です");
+					//プレイヤーのバーストチェック
+					if (player.isBusted()) {
+						System.out.println("残念、バーストしてしまいました。ディーラーの勝ちです。");
+						return;
+					}
+				} else {
+					System.out.println("あなたの入力は"
+							+ str2 + "です。y か n を入力してください。");
+				}
+			}
+		}
 
-	    //プレイヤーがカードを引くフェーズ
-	    try (Scanner scan2 = new Scanner(System.in);) {
-	      while (true) {
-	        System.out.println("カードを引きますか？ Yes:y or No:n");
-	        //キーボードの入力を受け付けて、変数strに代入する
-	        String str2 = scan2.next();
+		System.out.println("ディーラーの2枚目のカードは" + dealer.get(1) + "です");
+		//ディーラーが手札を17以上にするまでカードを引くフェーズ
+		while (dealer.sumPoint() < 17) {
+			//手札に山札から1枚加える
+			int index = dealer.add(deck.pop());
+			System.out.println("ディーラーの" + (index + 1) + "枚目のカードは" + dealer.get(index) + "です");
+			//ディーラーのバーストチェック
+			if (dealer.isBusted()) {
+				System.out.println("ディーラーがバーストしました。あなたの勝ちです！");
+				return;
+			}
+		}
 
-	        if ("n".equals(str2)) {
-	          break;
-	        } else if ("y".equals(str2)) {
-	          //手札に山札から1枚加える
-	          int index = player.add(deck.pop());
-	          System.out.println("あなたの" + (index+1) + "枚目のカードは" + player.get(index) + "です");
-	          System.out.println("現在の合計は" + player.sumPoint() + "です");
-	          //プレイヤーのバーストチェック
-	          if (player.isBusted()) {
-	            System.out.println("残念、バーストしてしまいました。ディーラーの勝ちです。");
-	            return;
-	          }
-	        } else {
-	          System.out.println("あなたの入力は"
-	              + str2 + "です。y か n を入力してください。");
-	        }
-	      }
-	    }
+		//ポイントを比較する
+		System.out.println("あなたのポイントは" + player.sumPoint() + "です");
+		System.out.println("ディーラーのポイントは" + dealer.sumPoint() + "です");
 
-	    System.out.println("ディーラーの2枚目のカードは" + dealer.get(1) + "です");
-	    //ディーラーが手札を17以上にするまでカードを引くフェーズ
-	    while (dealer.sumPoint() < 17) {
-	      //手札に山札から1枚加える
-	      int index = dealer.add(deck.pop());
-	      System.out.println("ディーラーの" + (index+1) + "枚目のカードは" + dealer.get(index) + "です");
-	      //ディーラーのバーストチェック
-	      if (dealer.isBusted()) {
-	        System.out.println("ディーラーがバーストしました。あなたの勝ちです！");
-	        return;
-	      }
-	    }
-
-	    //ポイントを比較する
-	    System.out.println("あなたのポイントは" + player.sumPoint() + "です");
-	    System.out.println("ディーラーのポイントは" + dealer.sumPoint() + "です");
-
-	    if ( player.sumPoint() == dealer.sumPoint()) {
-	      System.out.println("引き分けです。");
-	    } else if ( player.sumPoint() > dealer.sumPoint()) {
-	      System.out.println("勝ちました！");
-	    } else {
-	      System.out.println("負けました・・・");
-	    }
-	  }
+		if (player.sumPoint() == dealer.sumPoint()) {
+			System.out.println("引き分けです。");
+		} else if (player.sumPoint() > dealer.sumPoint()) {
+			System.out.println("勝ちました！");
+		} else {
+			System.out.println("負けました・・・");
+		}
 	}
+}
--- black_jack2/src/black_jack/Player.java
+++ black_jack2/src/black_jack/Player.java
@@ -5,61 +5,74 @@ import java.util.List;
 
 class Player {
 
-	private int cash; //手持ちのポイント
+	private int cash; //手持ち金
 	private int bet; //掛け金
 
+//	プレイヤーのコンストラクタ
 	public Player() {
-		cash = 20000; //プレイヤーの手持ちの初期値は20000;
+		cash = 20000; //プレイヤーの手持ち金の初期値は20000;
 		bet = 1000; //プレイヤーの賭け金の初期値は1000;
 	}
 
-	public Player(int dealerPoint,int dealerBet ) {
-		cash = dealerPoint; //ディーラーの手持ちの初期値は0;
+	public Player(int dealerPoint, int dealerBet) {
+		cash = dealerPoint; //ディーラーの手持ち金の初期値は0;
 		bet = dealerBet; //ディーラーに賭け金は存在しないので0に設定;
 	}
 
+	private List<Card> cardList = new ArrayList<>(); //手札
 
-	  private List<Card> cardList = new ArrayList<>(); //手札
-	  /**
-	   * カード追加
-	   * @param card
-	   * @return 最後に追加したindex (0-)
-	   */
-	  int add(Card card) {
-	    cardList.add(card);
-	    return cardList.size() - 1;
-	  }
-	  /**
-	   * カード読出
-	   * @param index (0-)
-	   * @return
-	   */
-	  Card get(int index) {
-	    return cardList.get(index);
-	  }
-	  /**
-	   * 現在の合計ポイント
-	   */
-	  int sumPoint() {
-	    int sum = 0;
-	    for(Card card : cardList) sum += card.getPoint();
-	    if(hasAce() && sum+10 <= 21) sum += 10;
-	    return sum;
-	  }
-	  /** A を持っているか */
-	  private boolean hasAce() {
-	    for(Card card : cardList) {
-	      if(card.getPoint() == 1) return true;
-	    }
-	    return false;
-	  }
-	  /**
-	   * 手札がバーストしているか
-	   */
-	  boolean isBusted() {
-	    return sumPoint() > 21;
-	  }
-
-
+	/**
+	 * カード追加
+	 * @param card
+	 * @return 最後に追加したindex (0-)
+	 */
+	int add(Card card) {
+		cardList.add(card);
+		return cardList.size() - 1;
 	}
 
+	/**
+	 * カード読出
+	 * @param index (0-)
+	 * @return
+	 */
+	Card get(int index) {
+		return cardList.get(index);
+	}
+
+	/**
+	 * 現在の合計ポイント
+	 */
+	int sumPoint() {
+		int sum = 0;
+		for (Card card : cardList)
+			sum += card.getPoint();
+		if (hasAce() && sum + 10 <= 21)
+			sum += 10;
+		return sum;
+	}
+
+	/** A を持っているか */
+	private boolean hasAce() {
+		for (Card card : cardList) {
+			if (card.getPoint() == 1)
+				return true;
+		}
+		return false;
+	}
+
+	/**
+	 * 手札がバーストしているか
+	 */
+	boolean isBusted() {
+		return sumPoint() > 21;
+	}
+
+	/**
+	 * 現在の手持ち金
+	 */
+	int nowCash() {
+		return cash;
+	}
+
+}
