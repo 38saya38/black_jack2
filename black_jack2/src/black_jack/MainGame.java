50fe3ef10b0b1cb5bb0777488391e5d9adb4d406
sayachan <sayachan@localhost>
2020-05-17 20:09:53 +0900

    ゲームの繰り返し処理を追加

--- black_jack2/src/black_jack/MainGame.java
+++ black_jack2/src/black_jack/MainGame.java
@@ -12,6 +12,7 @@ public class MainGame {
 		Player player = new Player(20000); //プレイヤーを作成し初期値を設定
 		Player dealer = new Player(); //ディーラーを作成し初期値を設定
 
+		while(1000 < player.nowCash()) {
 		System.out.println("現在の手持ちは" + player.nowCash() + "です。賭け金を決めてください");
 
 		try {
@@ -101,5 +102,7 @@ public class MainGame {
 		} else {
 			System.out.println("負けました・・・");
 		}
+	}
+		System.out.println("手持ち金が1000以下になりました。ゲームを終了します");
 	}
 }
